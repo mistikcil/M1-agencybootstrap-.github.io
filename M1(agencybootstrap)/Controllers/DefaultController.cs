2779fe6273469f4518cebf6c701fef6a2af64716
Merve <Merve@MERVE>
2020-03-20 04:41:03 +0300

    Add project files.

---
+++ M1(agencybootstrap)/Controllers/DefaultController.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace M1_agencybootstrap_.Controllers
+{
+    public class DefaultController : Controller
+    {
+        // GET: Default
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult Hakkimizda()
+        {
+            return View();
+        }
+        public ActionResult Takim()
+        {
+            return View();
+        }
+        public ActionResult Servis()
+        {
+            return View();
+        }
+        public ActionResult Portfoy()
+        {
+            return View();
+        }
+    }
+}
